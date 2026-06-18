9c395bd12cfc35b943a0f32e052a605f9be1828c
Maaz Khokhar <khokharmaaz@gmail.com>
2026-06-18 18:52:16 -0500

    dumb ahh syntax error

--- modules/virtualization/qemu.nix
+++ modules/virtualization/qemu.nix
@@ -6,27 +6,26 @@
 }:
 
 {
-    virtualisation.libvirtd.enable = true;
-    programs.virt-manager.enable = true;
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
 
-    virtualisation.libvirtd.qemu = {
-      ovmf.enable = true;
-      swtpm.enable = true;
-    };
-
-    boot.kernelModules = [
-      "kvm-intel"
-      "kvm-amd"
-    ];
-
-    users.users.river.extraGroups = [
-      "libvirtd"
-      "kvm"
-    ];
-
-    environment.systemPackages = with pkgs; [
-      virt-manager
-      qemu
-    ];
+  virtualisation.libvirtd.qemu = {
+    ovmf.enable = true;
+    swtpm.enable = true;
   };
 
+  boot.kernelModules = [
+    "kvm-intel"
+    "kvm-amd"
+  ];
+
+  users.users.river.extraGroups = [
+    "libvirtd"
+    "kvm"
+  ];
+
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    qemu
+  ];
+}
