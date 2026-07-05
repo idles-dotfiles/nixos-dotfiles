b1c21ee77dc7911eab56e5feb153bed41d95de68
Maaz Khokhar <khokharmaaz@gmail.com>
2026-07-04 23:23:03 -0500

    testing auto signing...

--- home/aariz/default.nix
+++ home/aariz/default.nix
@@ -2,8 +2,10 @@
 
 {
   home.username = "aariz";
+
   home.homeDirectory = "/home/aariz";
-  home.stateVersion = "26.05"; 
+
+  home.stateVersion = "26.05";
 
   programs.home-manager.enable = true;
 }
