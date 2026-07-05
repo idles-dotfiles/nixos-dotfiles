12bd99e84ca9760aecf9c6ccbe091e257e923aaf
Maaz Khokhar <khokharmaaz@gmail.com>
2026-07-04 23:21:39 -0500

    test signing?

--- home/river/programs/jujutsu.nix
+++ home/river/programs/jujutsu.nix
@@ -8,11 +8,13 @@
         email = "khokharmaaz@gmail.com";
         name = "Maaz Khokhar";
       };
+
       signing = {
         behavior = "own";
         backend = "gpg";
         key = "A8D1AEA9C92D6867";
       };
+
       ui.color = "always";
     };
   };
