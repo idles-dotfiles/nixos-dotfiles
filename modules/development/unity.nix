9bf7f7a2d50b329f69aa3009f76bec5387776d24
Maaz Khokhar <khokharmaaz@gmail.com>
2026-07-22 16:16:53 -0500

    added some helper dependencies (for unity)

--- modules/development/unity.nix
+++ modules/development/unity.nix
@@ -11,6 +11,18 @@ in
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
       unityhub
+      # Required for Unity/C# development
+      dotnetCorePackages.dotnet_9.sdk
+
+      # IDE for Development
+      vscode
+
+      # Mono and MSBuild (often needed for Unity integration)
+      mono
+      msbuild
+
+      # NuGet (for dependency management)
+      dotnetPackages.Nuget
     ];
   };
 }
